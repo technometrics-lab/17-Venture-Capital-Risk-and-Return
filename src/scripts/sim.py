d6192074ec87f6b35c713b9e4fb6a6ebe754a254
Vekkt <francois.burguet@epfl.ch>
2022-06-17 13:51:59 +0200

    improved probability distribution

--- src/scripts/sim.py
+++ src/scripts/sim.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scripts.utils import *
+from scipy.stats import lognorm
 
 
-def sim(gamma, delta, sigma, k, a, b, c, d, pim, start_date, logrf, logmk, logv, stockidx, dok, big, sample_size=54):
+def sim(gamma, delta, sigma, k, a, b, c, d, pi_err, start_date, logrf, logmk, logv, stockidx, use_k, big, sample_size=54):
     def pipo_func(x):
         return 1 / (1 + np.exp(-a * (x - b)))
 
-    assert np.log(k) >= logv[0], 'sim ERROR: k off value grid'
     T = sample_size - start_date - 1
     N = logv.shape[0]
 
@@ -30,17 +30,12 @@ def sim(gamma, delta, sigma, k, a, b, c, d, pim, start_date, logrf, logmk, logv,
     val_max = logv[-1]
     val_step = (val_max - val_min) / (N - 1)
 
-    if start_date == -1 and stockidx > 0:
+    if stockidx == 0:
         prob_lnV = np.zeros(N)
-        prob_lnV += (np.exp(-0.5 * (gamma + logrf.mean() + delta * (logmk.mean() - logrf.mean())) ** 2 /
-                            (delta ** 2 * logmk.std() ** 2 + sigma ** 2)))
-        prob_lnV /= prob_lnV.sum()
-    elif stockidx == 0:
-        prob_lnV = np.zeros(N)
-        prob_lnV = np.exp(-0.5 * gamma ** 2 / sigma ** 2)
+        prob_lnV = np.exp(-0.5 * gamma**2 / sigma**2)
         prob_lnV /= prob_lnV.sum()
 
-    if dok:
+    if use_k:
         prob_bank = (logv <= np.log(k)) * (1 - (np.exp(logv) - np.exp(logv[0])) / (k - np.exp(logv[0])))
     else:
         prob_bank = 0
@@ -56,9 +51,11 @@ def sim(gamma, delta, sigma, k, a, b, c, d, pim, start_date, logrf, logmk, logv,
     for t in range(T):
         if start_date >= 0 and stockidx > 0:
             mu = gamma + logrf[start_date+t+1] + delta * (logmk[start_date+t+1] - logrf[start_date+t+1])
-            prob_lnV0 = (np.exp(-(dlogV - mu)**2 / (2 * sigma**2)) / (np.exp(dlogV) * sigma * np.sqrt(2 * np.pi)))
-            prob_lnV0 = prob_lnV0[:-1] * np.diff(exp(dlogV))
-            # OLD AND BAD: prob_lnV0 /= prob_lnV0.sum(), some sort of KDE ??
+            prob_lnV0 = np.diff(lognorm.cdf(np.exp(dlogV), scale=np.exp(mu), s=sigma))
+            # OLD AND BAD:
+            # prob_lnV0 = np.exp(-(dlogV - mu)**2 / (2 * sigma**2))
+            # prob_lnV0 /= prob_lnV0.sum()
+            
             
             for i in range(N):
                 s = prob_lnV0[N - i - 1: 2 * N - i - 1]
@@ -72,7 +69,7 @@ def sim(gamma, delta, sigma, k, a, b, c, d, pim, start_date, logrf, logmk, logv,
         phadbk = prob_val * (1 - prob_exit) * prob_bank
         prob_private = prob_val * (1 - prob_exit) * (1 - prob_bank)
 
-        prob_ipo_obs[t, :] = (1 - pim) * d * phadip + pim * d * phadip.mean()
+        prob_ipo_obs[t, :] = (1 - pi_err) * d * phadip + pi_err * d * phadip.mean()
         if big:
             prob_ipo_hid[t, :] = (1 - d) * phadip
             prob_bkp_obs[t, :] = c * phadbk
@@ -91,5 +88,5 @@ def sim(gamma, delta, sigma, k, a, b, c, d, pim, start_date, logrf, logmk, logv,
     prob_pvt /= 1e4
 
     probsum = prob_pvt[-1] + prob_ipo_obs.sum() + prob_ipo_hid.sum() + prob_bkp_obs.sum() + prob_bkp_hid.sum();
-    assert abs(probsum - 1) < 1e-6, f"sim3 WARNING: Probabilities sum to more or less than one ({probsum:.4f})"
+    assert abs(probsum - 1) < 1e-6, f"sim3 ERROR: Probabilities sum to more or less than one ({probsum:.4f})"
     return prob_pvt, prob_ipo_obs, prob_ipo_hid, prob_bkp_obs, prob_bkp_hid
