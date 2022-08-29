0617ac23ef80818315fef43bd976325648e76b04
Vekkt <francois.burguet@epfl.ch>
2022-08-29 11:20:41 +0200

    comments

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -5,6 +5,15 @@ from scripts.minimize_lkh import Model
 from scripts.utils import display_return_stats, find_case, transform_params, save_results
 
 def get_dates(x, test=False):
+    """Returns the start and end date for the given dataset.
+
+    Args:
+        x (DataFrame): dataset of observations
+        test (bool, optional): Whether the dataset is Cochrane test set or not. Defaults to False.
+
+    Returns:
+        (str, str): begining and end dates, format yy-mm-dd
+    """    
     start_date, end_date = min(x.round_date), max(x.round_date)
     if test:
         s, e = str(start_date), str(end_date)
@@ -14,6 +23,27 @@ def get_dates(x, test=False):
 
 
 def start_opti(x, xc, params, impose_alpha=False, stockidx=1, nopi=0, use_k=1, test=False, maxiter=30, benchmark='^SP500TR'):
+    """Starts the MLE procedure, given an initial dataset. Automatically infers dates, downloads market data, and pre-process VC data.
+    Expected parameters are: gamma, delta, sigma, k, a, b, pi, used as initial values for the optimization.
+    Data for risk free rate is the 3 month t-bill from FRED (quarterly)
+    Data for public equity indices are fetched from yahoo finance.
+    The main dataset columns must be: company_id, group, segment, round_date, raised_usd, postvalue_usd, exit_date, exit_type, exit_value, return_usd
+
+    Args:
+        x (DataFrame): dataset of observations
+        xc (DataFrame): categories for each observations (between 1 and 6)
+        params (list): list of parameters for the `find_likelihood` function
+        impose_alpha (bool, optional): Test for alpha=0. Defaults to False.
+        stockidx (int, optional): Use market as benchmark. Defaults to 1.
+        nopi (int, optional): Use error detection (pi). Defaults to 0.
+        use_k (int, optional): Account for failures. Defaults to 1.
+        test (bool, optional): Test set or true dataset. Defaults to False.
+        maxiter (int, optional): Maximum number of iterations for the minimization function. Defaults to 30.
+        benchmark (str, optional): Which market benchmark to use. Defaults to `^SP500TR`.
+
+    Returns:
+        DataFrame: Result dataframe containing parameter values and standard errors
+    """    
     start_date, end_date = get_dates(x, test)
     size = (end_date.to_period(freq='Q') - start_date.to_period(freq='Q')).n + 2
     start = start_date.strftime('%Y-%m-%d')
@@ -28,7 +58,7 @@ def start_opti(x, xc, params, impose_alpha=False, stockidx=1, nopi=0, use_k=1, t
 
     c, d = display_return_stats(x)
 
-    minage = 0.25
+    minage = 0.25 # minimum round age since beginning of observations (in quarter). Minimum is 0.25 (1 quarter)
     logv = np.arange(-7, 7.1, 0.1)
     params[-1] = 0 if nopi == 1 else params[-1]
     mask = [impose_alpha != 1, stockidx is not None, True, use_k != 0, True, True, nopi != 1]
@@ -39,6 +69,19 @@ def start_opti(x, xc, params, impose_alpha=False, stockidx=1, nopi=0, use_k=1, t
         
         
 def run_bootstrap(x, xc, params0, n_bstrap, test, benchmark):
+    """Runs bootstrap iterations
+
+    Args:
+        x (DataFrame): dataset of observations
+        xc (DataFrame): categories for each observations (between 1 and 6)
+        params (list): list of parameters for the find_likelihood function
+        n_bstrap (int): number of bootstrap iterations to perform
+        test (boolean): use Cochrane test dataset or not
+        benchmark (str): Which market benchmark to use. Defaults to '^SP500TR'.
+
+    Returns:
+        dict: dictionnary containing all bootstrap results, not aggregated
+    """    
     frac = 0.8
     res = {}
     for i in range(n_bstrap):
@@ -58,7 +101,7 @@ def run_bootstrap(x, xc, params0, n_bstrap, test, benchmark):
 
 
 def main(filepath, params0=None, from_date=None, to_date=None, industries=None, test=False, bootstrap=False, 
-         pred=True, use_k=True, use_bkp=True, n_bstrap=20, maxiter=30, benchmark='^SP500TR'):
+         pred=True, use_k=True, use_bkp=True, n_bstrap=100, maxiter=30, benchmark='^SP500TR'):
     
     if params0 is None:
         gamma0 = 0.01
@@ -138,4 +181,4 @@ if __name__ == "__main__":
     benchmark = '^RUT'
     main('security_data.csv', from_date='2010-01-01', benchmark=benchmark)
     main('security_data.csv', from_date='2010-01-01', bootstrap=True, benchmark=benchmark)
- 
+   
